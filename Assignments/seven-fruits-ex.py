f56d368659b5900a87eda09f666c2d417905b891
haidar-ims <haidar.ihsan@gmail.com>
2025-10-16 11:43:54 +0800

    First Assignment

---
+++ Assignments/seven-fruits-ex.py
@@ -0,0 +1,6 @@
+# Write a program to store seven fruits in a list entered by the user.
+
+list_fruits = ()
+for i in range (0,6):
+    list_fruits = ('Apple, orange, manggo, cherry, kiwi, Strawberry, Banana')
+    print (list_fruits)
