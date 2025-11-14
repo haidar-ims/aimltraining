21362eacb305a61d40600181d963b94002c52301
haidar-ims <haidar.ihsan@gmail.com>
2025-11-14 22:28:06 +0800

    Day 29

---
+++ Day-28/DirectQuery.sql
@@ -0,0 +1,11 @@
+use OurDb
+select * from category order by CatId
+select * from Product
+
+use SalesDb
+select * from Sales
+select * from Products
+insert into Sales values (1, 'North',4,'2025-11-13')
+
+DELETE FROM Sales
+WHERE SalesID = 1018
